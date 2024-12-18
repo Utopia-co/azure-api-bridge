6b3a0a0f78038a0701f68f048d066dad552c96a9
cauansousa <cauanvinicius00@gmail.com>
2024-12-17 23:48:01 -0300

    Delete ref.py

--- ref.py
+++
@@ -1,9 +0,0 @@
-import os
-import json
-from openai import AzureOpenAI
-    
-client = AzureOpenAI(
-    api_key=os.getenv("AZURE_OPENAI_API_KEY"),  
-    api_version="2024-05-01-preview",
-    azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
-    )
